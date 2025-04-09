87d160be6be9d91a9cb8d8c08a9ad13f8b12feff
Jack Nieland <jack@jacknieland.com>
2025-04-09 05:32:07 +0000

    Muliply

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route('/', methods=['GET', 'POST'])
+def multiply():
+    result = None
+    if request.method == 'POST':
+        try:
+            num1 = int(request.form['num1'])
+            num2 = int(request.form['num2'])
+            result = num1 * num2
+        except (ValueError, KeyError):
+            result = "Invalid input"
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
